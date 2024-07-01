dc4701a294a357333d76a35f1001cf89effc5adc
Migue <miguelulmi@hotmail.com>
2024-07-01 03:06:16 -0300

    Avances en la pagina
    Se pulieron detalles de diseño, se hizo la pagina de desencriptado y se empezó con el javascript

---
+++ app.js
@@ -0,0 +1,60 @@
+let texto_a_Encriptar = [];
+let texto_Encriptado = [];
+let texto_a_Desencriptar = [];
+let texto_Desencriptado = [];
+
+function imprimir_En_Izquierdo(letra){
+
+}
+
+function imprimir_En_Derecho(letra){
+    
+}
+
+function encripto_A ()[
+
+]
+
+function encripto_E ()[
+    
+]
+
+function encripto_I ()[
+    
+]
+
+function encripto_O ()[
+    
+]
+
+function encripto_U ()[
+    
+]
+
+function desencripto_A ()[
+
+]
+
+function desencripto_E ()[
+
+]
+
+function desencripto_I ()[
+
+]
+
+function desencripto_O ()[
+
+]
+
+function desencripto_U ()[
+
+]
+
+function encriptar (texto, longitud){
+
+}
+
+function desencriptar (texto, longitud){
+
+}
