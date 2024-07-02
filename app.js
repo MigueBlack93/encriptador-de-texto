49cb69f9964ddf51099fb7f15ca9db2a234055fa
Migue <miguelulmi@hotmail.com>
2024-07-01 23:17:11 -0300

    empezando logica

--- app.js
+++ app.js
@@ -3,58 +3,42 @@ let texto_Encriptado = [];
 let texto_a_Desencriptar = [];
 let texto_Desencriptado = [];
 
-function imprimir_En_Izquierdo(letra){
-
-}
-
 function imprimir_En_Derecho(letra){
     
 }
 
-function encripto_A ()[
+function encripto (vocal){
+    if (vocal === "a"){
+        return "ai";
+    }
+    if (vocal === "e"){
+        return "enter";
+    }
+    if (vocal === "i"){
+        return "imes";
+    }
+    if (vocal === "o"){
+        return "ober";
+    }
+    if (vocal === "u"){
+        return "ufat";
+    }
+}
 
-]
+function encriptar (){
+    let texto_a_Encriptar = document.getElementById("main__ingreso__textarea").value.split("",undefined);
+    let texto_Encriptado = [];
 
-function encripto_E ()[
-    
-]
-
-function encripto_I ()[
-    
-]
-
-function encripto_O ()[
-    
-]
-
-function encripto_U ()[
-    
-]
-
-function desencripto_A ()[
-
-]
-
-function desencripto_E ()[
-
-]
-
-function desencripto_I ()[
-
-]
-
-function desencripto_O ()[
-
-]
-
-function desencripto_U ()[
-
-]
-
-function encriptar (texto, longitud){
+    for (contador=0;contador<texto_a_Encriptar.length;contador++){
+        if (texto_a_Encriptar[contador]==="a"){
+            texto_Encriptado[contador] = "ai";
+        }
+        else {
+            texto_Encriptado[contador] = texto_a_Encriptar[contador];
+        }
+        console.log(texto_Encriptado[contador]);
+    }
 
 }
 
-function desencriptar (texto, longitud){
-
-}
+console.log(texto_Encriptado);
