5faabfc23a099c863bba71c513f034083bfbc941
Migue <miguelulmi@hotmail.com>
2024-07-03 14:25:43 -0300

    Ahora si funciona los caracteres no admitidos
    seeee

--- app.js
+++ app.js
@@ -6,6 +6,37 @@ let caracteresNoAdmitidos = [];
 let sumatoriaCaracteresNoAdmitidos = 0;
 let solamenteCaracteres = "¡Caracter no admitido! Solamente letras minúsculas y sin acentos.";
 
+function cargarCaracteresNoAdmitidos(desde, hasta){
+    for(i=desde;i<=hasta;i++){
+        caracteresNoAdmitidos[sumatoriaCaracteresNoAdmitidos] = String.fromCharCode(i);
+        sumatoriaCaracteresNoAdmitidos++;
+        console.log (caracteresNoAdmitidos);
+    }
+}
+
+cargarCaracteresNoAdmitidos(33, 43);
+cargarCaracteresNoAdmitidos(45, 45);
+cargarCaracteresNoAdmitidos(47, 96);
+cargarCaracteresNoAdmitidos(123,255);
+
+let caracter = document.getElementById('main-ingreso-textarea');
+caracter.addEventListener('keydown', function(event) {
+        if(event.key.charCodeAt(0)<33||event.key.charCodeAt(0)==44||event.key.charCodeAt(0)==46||(event.key.charCodeAt(0)>96&&event.key.charCodeAt(0)<123)){
+            return 0;
+        }
+        else{
+            alert(solamenteCaracteres);
+        }
+
+
+
+    // Aquí puedes manejar la lógica para la tecla presionada
+    console.log('Tecla presionada:', event.key);
+    // Transforma la tecla en su código ASCII
+    const asciiCode = event.key.charCodeAt(0);
+    console.log('Código ASCII:', asciiCode);
+});
+
 function encripto (vocal){
     if (vocal === "a"){
         return "ai";
@@ -22,23 +53,14 @@ function encripto (vocal){
     if (vocal === "u"){
         return "ufat";
     }
-}
-
-function cargarCaracteresNoAdmitidos(desde, hasta){
-    for(i=desde;i<=hasta;i++){
-        caracteresNoAdmitidos[sumatoriaCaracteresNoAdmitidos] = String.fromCharCode(i);
-        sumatoriaCaracteresNoAdmitidos++;
+    else{
+        return vocal;
     }
 }
 
-cargarCaracteresNoAdmitidos(33, 43);
-cargarCaracteresNoAdmitidos(45, 45);
-cargarCaracteresNoAdmitidos(47, 96);
-cargarCaracteresNoAdmitidos(123,255);
-
 function caracterNoAdmitido(caracter){
     for(i=0;i<caracteresNoAdmitidos.length;i++){
-        if(caracter.key==caracteresNoAdmitidos[0]){
+        if(caracter.key==caracteresNoAdmitidos[i]){
             alert(solamenteCaracteres);
         }
         else{
