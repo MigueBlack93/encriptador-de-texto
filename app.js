7559747254b2e99f5c54bcf16c1a75bc72d5cbfc
Migue <miguelulmi@hotmail.com>
2024-07-04 07:31:28 -0300

    Funcionando varias cosas más
    Se perfecciono todo hasta encriptar, falta desencriptar

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ let texto_a_Encriptar = [];
 let texto_Encriptado = [];
 let texto_a_Desencriptar = [];
 let texto_Desencriptado = [];
+let solamenteCaracteres = "¡Caracter no admitido! Solamente letras minúsculas y sin acentos.";
+
+/*
 let caracteresNoAdmitidos = [];
 let sumatoriaCaracteresNoAdmitidos = 0;
-let solamenteCaracteres = "¡Caracter no admitido! Solamente letras minúsculas y sin acentos.";
 
 function cargarCaracteresNoAdmitidos(desde, hasta){
     for(i=desde;i<=hasta;i++){
@@ -18,23 +20,27 @@ cargarCaracteresNoAdmitidos(33, 43);
 cargarCaracteresNoAdmitidos(45, 45);
 cargarCaracteresNoAdmitidos(47, 96);
 cargarCaracteresNoAdmitidos(123,255);
+*/
 
 let caracter = document.getElementById('main-ingreso-textarea');
 caracter.addEventListener('keydown', function(event) {
-        if(event.key.charCodeAt(0)<33||event.key.charCodeAt(0)==44||event.key.charCodeAt(0)==46||(event.key.charCodeAt(0)>96&&event.key.charCodeAt(0)<123)){
-            return 0;
-        }
-        else{
-            alert(solamenteCaracteres);
-        }
 
+    let evento = event.key.charCodeAt(0);
+    let carac = event.key;
 
+        // Aquí puedes manejar la lógica para la tecla presionada
+        console.log('Tecla presionada:', event);
+        // Transforma la tecla en su código ASCII
+        console.log('Código ASCII:', evento);
 
-    // Aquí puedes manejar la lógica para la tecla presionada
-    console.log('Tecla presionada:', event.key);
-    // Transforma la tecla en su código ASCII
-    const asciiCode = event.key.charCodeAt(0);
-    console.log('Código ASCII:', asciiCode);
+    if((evento > 33 && evento < 45|| evento == 45 || evento > 47 && evento < 96 || evento > 123) && carac !== 'Enter' && carac !== 'Shift' && carac !== 'Backspace' && carac !== 'ArrowUp' && carac !== 'ArrowRight' && carac !== 'ArrowDown' && carac !== 'ArrowLeft' && carac !== 'Tab'){
+        alert(solamenteCaracteres);
+        event.preventDefault();
+        return;
+    }
+    else{
+        return 0;
+    }
 });
 
 function encripto (vocal){
@@ -58,31 +64,14 @@ function encripto (vocal){
     }
 }
 
-function caracterNoAdmitido(caracter){
-    for(i=0;i<caracteresNoAdmitidos.length;i++){
-        if(caracter.key==caracteresNoAdmitidos[i]){
-            alert(solamenteCaracteres);
-        }
-        else{
-            return caracter;
-        }
-    }
-}
-
 function encriptar (){
-    let texto_a_Encriptar = document.getElementById("main__ingreso__textarea").value.split("",undefined);
+    let texto_a_Encriptar = document.getElementById("main-ingreso-textarea").value.split("",undefined);
     let texto_Encriptado = [];
 
-    for (contador=0;contador<texto_a_Encriptar.length;contador++){
-        if (texto_a_Encriptar[contador]==="a"){
-            texto_Encriptado[contador] = "ai";
-        }
-        else {
-            texto_Encriptado[contador] = texto_a_Encriptar[contador];
-        }
-        console.log(texto_Encriptado[contador]);
+    for (i=0;i<texto_a_Encriptar.length;i++){
+        
+        texto_Encriptado[i] = encripto(texto_a_Encriptar[i]);
+
+        console.log(texto_Encriptado[i]);
     }
-
 }
-
-console.log(texto_Encriptado);
